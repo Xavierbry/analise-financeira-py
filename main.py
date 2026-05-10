059b1f91b605e9f9b2c0ca2f447f751d18c057a7
Xavierbry <xaviergabrielab@outlook.com>
2026-05-09 22:28:29 -0300

    style: cards na tela incial, descrição e temma dark

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from orcamento import calcular_orcamento
 from cartao import analisar_cartao
 from simulador import simular_compra
 
+ctk.set_appearance_mode("dark") # ativa o tema dark
+ctk.set_default_color_theme("dark-blue") # cor azul dos botões
 app = ctk.CTk()
 app.title("Análise Financeira")
-app.geometry("400x550")
+app.geometry("400x630")
 
 
 def limpar_tela():
@@ -18,12 +20,40 @@ def mostrar_menu():
 
     titulo = ctk.CTkLabel(app, text="Análise Financeira", font=("Arial", 20))
     titulo.pack(pady=20)
-    msg = ctk.CTkLabel(app, text="Cada realidade financeira é diferente. Use esta regra como referência e adapte conforme suas prioridades.", font=("Arial", 14, "italic"), wraplength=350, justify="center")
+    msg = ctk.CTkLabel(app, text="Cada realidade financeira é diferente. Use esta regra como referência e adapte conforme suas prioridades.", font=("Arial", 12, "italic"), wraplength=350, justify="center")
     msg.pack(pady=10)
 
-    ctk.CTkButton(app, text="Orçamento", command=tela_orcamento).pack(pady=10)
-    ctk.CTkButton(app, text="Cartão de Crédito", command=tela_cartao).pack(pady=10)
-    ctk.CTkButton(app, text="Simular Compra", command=tela_simulador).pack(pady=10)
+    # botão dentro de um Frame
+
+    #  botão orçamento
+    frame = ctk.CTkFrame(app, fg_color="#63635f", corner_radius=12)
+    frame.pack(pady=10, padx=20, fill="x")
+
+    ctk.CTkButton(frame, text="Orçamento", font=("Arial", 14, "bold"), command=tela_orcamento).pack(anchor="w",pady=10, padx=10)
+    ctk.CTkLabel(frame, text="Regra 50-30-20 aplicada à sua renda", font=("Arial", 12), text_color="gray").pack(anchor="w", padx=14)
+    ctk.CTkButton(frame, text="Acessar →", command=tela_orcamento, width=100).pack(anchor="w", padx=14, pady=(8, 14))
+
+
+    #botão cartão de crédito
+    frame = ctk.CTkFrame(app, fg_color="#63635f", corner_radius=12)
+    frame.pack(pady=10, padx=20, fill="x")
+    
+    ctk.CTkButton(frame, text="Cartão de Crédito",font=("Arial", 14, "bold"), command=tela_cartao).pack(anchor="w", pady=10, padx=10)
+    ctk.CTkLabel(frame, text="Seu limite vs. sua renda", font=("Arial", 12), text_color="gray").pack(anchor="w", padx=14)
+    ctk.CTkButton(frame, text="Acessar →", command=tela_cartao, width=100).pack(anchor="w", padx=14, pady=(8, 14))
+
+
+    #botao simulador de compra
+    frame = ctk.CTkFrame(app, fg_color="#63635f", corner_radius=12)
+    frame.pack(pady=10, padx=20, fill="x")
+    ctk.CTkButton(frame, text="Simular Compra", font=("Arial", 14, "bold"), command=tela_simulador).pack(anchor="w", pady=10, padx=10)
+    ctk.CTkLabel(frame, text="Calcule o parcelamento ideal para não comprometer sua renda", font=("Arial", 12), text_color="gray").pack(anchor="w", padx=14)
+    ctk.CTkButton(frame, text="Acessar →", command=tela_simulador, width=100).pack(anchor="w", padx=14, pady=(8, 14))
+
+    #botões soltos na tela
+    #ctk.CTkButton(app, text="Orçamento", command=tela_orcamento).pack(pady=10)
+    #ctk.CTkButton(app, text="Cartão de Crédito", command=tela_cartao).pack(pady=10)
+    #ctk.CTkButton(app, text="Simular Compra", command=tela_simulador).pack(pady=10)
 
     
 # TELA ORÇAMENTO
@@ -31,7 +61,7 @@ def mostrar_menu():
 def tela_orcamento():
     limpar_tela()
 
-    ctk.CTkLabel(app, text="Digite sua renda").pack(pady=10) #cria e posiciona o texto
+    ctk.CTkLabel(app, text="Digite sua renda", font=("Arial", 14)).pack(pady=10) #cria e posiciona o texto
     entry = ctk.CTkEntry(app) # CTkEntry seria o campo de entrada como um input 
     entry.pack(pady=10) #mostra o campo da tela
 
@@ -81,11 +111,11 @@ A regra 50/30/20 é uma estratégia simples e eficaz para organizar suas finanç
 def tela_cartao():
     limpar_tela()
 
-    ctk.CTkLabel(app, text="Digite sua renda").pack(pady=5)
+    ctk.CTkLabel(app, text="Digite sua renda", font=("Arial", 14)).pack(pady=5)
     entry_renda = ctk.CTkEntry(app)
     entry_renda.pack(pady=5)
 
-    ctk.CTkLabel(app, text="Limite do cartão").pack(pady=5)
+    ctk.CTkLabel(app, text="Limite do cartão", font=("Arial", 14)).pack(pady=5)
     entry_limite = ctk.CTkEntry(app)
     entry_limite.pack(pady=5)
 
@@ -128,11 +158,11 @@ Dica: {dados['dica']}
 def tela_simulador():
     limpar_tela()
 
-    ctk.CTkLabel(app, text="Digite sua renda").pack(pady=5)
+    ctk.CTkLabel(app, text="Digite sua renda", font=("Arial", 14)).pack(pady=5)
     entry_renda = ctk.CTkEntry(app)
     entry_renda.pack(pady=5)
 
-    ctk.CTkLabel(app, text="Valor do produto").pack(pady=5)
+    ctk.CTkLabel(app, text="Valor do produto", font=("Arial", 14)).pack(pady=5)
     entry_valor = ctk.CTkEntry(app)
     entry_valor.pack(pady=5)
 
